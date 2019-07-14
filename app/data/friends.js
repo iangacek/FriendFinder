197d634d741abeb2d8818c4e321bdb08d440c260
Ian Gacek <gacek004@umn.edu>
2019-07-14 16:13:04 -0500

    .

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArray = [
+	{
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+	{
+		name: 'Nick',
+		photo: '',
+		scores: [
+            5,
+            2,
+            5,
+            5,
+            3,
+            1,
+            2,
+            3,
+            4,
+            2
+          ]
+	},
+	{
+		name: 'Josh',
+		photo: '',
+		scores: [
+            4,
+            2,
+            4,
+            3,
+            4,
+            2,
+            3,
+            4,
+            5,
+            2
+          ]
+	},
+	{
+		name: 'Stephen',
+		photo: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_8_desktop_mobile/f_auto/vikings/i0zvbd69vp9haiegt34q.jpg',
+		scores: [
+            5,
+            4,
+            3,
+            2,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4
+          ]
+	},
+	{
+		name: 'Brandon',
+		photo: '',
+		scores: [
+            4,
+            2,
+            3,
+            5,
+            5,
+            2,
+            3,
+            4,
+            3,
+            2
+          ]
+    },
+];
+
+module.exports = friendsArray;
