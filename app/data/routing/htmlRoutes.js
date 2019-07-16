9919eb66ad1b63326012fa759bfc04bbbb01507a
Ian Gacek <gacek004@umn.edu>
2019-07-15 21:26:55 -0500

    Fixed home.html

--- app/data/routing/htmlRoutes.js
+++ app/data/routing/htmlRoutes.js
@@ -1,11 +1,12 @@
 var path = require('path');
 
 module.exports = function(app) {
-    app.get('/', function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
+        console.log("test");
     });
 
-    app.get('/survey', function(req, res) {
+    app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 };
