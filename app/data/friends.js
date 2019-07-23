419a8c6083225f428bfc5a11bdd62ac523754c7c
Ian Gacek <48931196+iangacek@users.noreply.github.com>
2019-07-23 10:01:05 -0500

    .

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [{
-            name: "Ahmed",
-            photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-            scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+            name: "Eric",
+            photo: "",
+            scores: [5, 3, 4, 1, 5, 1, 2, 5, 4, 1]
       },
       {
             name: 'Nick',
